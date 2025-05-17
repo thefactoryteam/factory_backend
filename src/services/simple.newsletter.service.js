0cce0f6eba54ab81fdbdf17913a0d39aebef1f26
thefactoryteam <thefactorydevteam@gmail.com>
2025-05-17 19:37:07 +0100

    Update simple.newsletter.service.js

--- src/services/simple.newsletter.service.js
+++ src/services/simple.newsletter.service.js
@@ -16,9 +16,9 @@ export const simpleNewsletterService = async ({ name, email }) => {
     const newSubscriber = await Newsletter.create({ name, email });
 
     const transporter = nodemailer.createTransport({
-        host: "smtp.hostinger.com",
-        port: 587,
-        // secure: true,
+        host: "smtp.titan.email",
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.HOSTINGER_EMAIL,
             pass: process.env.HOSTINGER_EMAIL_PASSWORD
